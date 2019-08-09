75161cca7c7639a77136bba72e5d245591f7a286
zhenik <nikita.zhevnitskiy@gmail.com>
2019-08-09 14:10:08 +0200

    rm examples

--- src/main/java/no/sysco/middleware/prometheus/kafka/example/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * This package contains classes with usage examples
- * that let you view the metrics reported by kafka clients
- *
- * todo: move this package our of library
- * @author <a href="https://github.com/zhenik">Nikita Zhevnitskiy</a>
- */
-package no.sysco.middleware.prometheus.kafka.example;
